9ad041a1f8ada7836a8cd8663c9abaeb3ec68d22
vennila-3 <113087858+vennila-3@users.noreply.github.com>
2022-09-10 11:13:41 +0530

    Update pattern print.java

--- BASIC/pattern print.java
+++ BASIC/pattern print.java
@@ -11,5 +11,18 @@ public class Main
 		    }
 		    System.out.print("\n");
 		}
+		System.out.print("\n");
+		for(int i=1;i<=5;i++)
+		{
+		    
+		    for(int j=5;j>=i;j--)
+		
+		    {
+		        System.out.print(j);
+		       
+		    }
+		    System.out.print("\n");
+		}
+		
 	}
 }
